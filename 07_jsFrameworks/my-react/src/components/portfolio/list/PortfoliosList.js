a6de79ee1477a9e9d0b16dd993e50337a3791ceb
Oleksandr Nykytin <keeper@ninydev.com>
2022-04-27 19:15:01 +0300

    Portfolio LocalStorage

--- 07_jsFrameworks/my-react/src/components/portfolio/list/PortfoliosList.js
+++ 07_jsFrameworks/my-react/src/components/portfolio/list/PortfoliosList.js
@@ -9,15 +9,42 @@ export default class PortfoliosList extends React.Component {
         super(props);
         this.state = {
             portfolios: [
-                {id: "1", name: "First Work"},
-                {id: "2", name: "Second Work"}
+                // {id: "1", name: "First Work"},
+                // {id: "2", name: "Second Work"}
             ],
             isLoad: true, // Показывает - произошла ли загрузка
             error: null // Сохраняет ошибку при загрузке
         }
 
         this.create = this.create.bind(this)
+        this.onLogAll = this.onLogAll.bind(this)
+
+        this.localSaveAll = this.localSaveAll.bind(this)
+        this.localLoadAll = this.localLoadAll.bind(this)
+        this.localClear = this.localClear.bind(this)
+        this.localClear = this.localClear.bind(this)
     }
+
+    onLogAll(){
+        console.log(this.state.portfolios)
+    }
+
+    localSaveAll(){
+        localStorage.setItem('ninyPortfolio', JSON.stringify(this.state.portfolios))
+    }
+    localLoadAll(){
+        const state = this.state
+        let str = localStorage.getItem('ninyPortfolio')
+        console.log(str)
+        if(!str) state.portfolios = []
+        else state.portfolios = JSON.parse(str)
+        this.setState(state)
+    }
+
+    localClear(){
+        localStorage.clear()
+    }
+
 
     create(newPortfolio){
         newPortfolio.id = Date.now()
@@ -32,6 +59,10 @@ export default class PortfoliosList extends React.Component {
         return (
             <div>
                 <h2> All List </h2>
+                <button onClick={this.onLogAll}> Console </button>
+                <button onClick={this.localSaveAll}> save all </button>
+                <button onClick={this.localLoadAll}> load all </button>
+                <button onClick={this.localClear}> clear </button>
                 <ul>
                     {this.state.portfolios.map(p=> {
                         return <PortfolioListItem portfolio={p} key={'portfolioListItem_' + p.id} />
