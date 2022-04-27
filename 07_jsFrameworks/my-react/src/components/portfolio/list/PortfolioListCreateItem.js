d52d4a6a848c555b80a0d7bf5b12059166950431
Oleksandr Nykytin <keeper@ninydev.com>
2022-04-27 20:51:40 +0300

    BackEnd Portfolio

--- 07_jsFrameworks/my-react/src/components/portfolio/list/PortfolioListCreateItem.js
+++ 07_jsFrameworks/my-react/src/components/portfolio/list/PortfolioListCreateItem.js
@@ -46,6 +46,24 @@ export default class PortfolioListCreateItem extends React.Component {
     }
 
     onSave(){
+
+        fetch('/api/portfolios/',
+            {
+                method: 'POST', // *GET, POST, PUT, DELETE, etc.
+                mode: 'cors', // no-cors, *cors, same-origin
+                cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+                credentials: 'same-origin', // include, *same-origin, omit
+                headers: {
+                    'Content-Type': 'application/json'
+                    // 'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                redirect: 'follow', // manual, *follow, error
+                referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+                body: JSON.stringify(this.state.portfolio) // body data type must match "Content-Type" header
+            })
+            .catch(err => {console.log(err)})
+
+
         this.props.onSave(this.state.portfolio)
         const state = this.state
         state.isVisible = !state.isVisible
