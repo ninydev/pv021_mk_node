4f6eb75ffb5c2c4d06ff27c10252bc02abca97e6
Oleksandr Nykytin <keeper@ninydev.com>
2022-04-27 19:54:12 +0300

    Portfolio Img

--- 07_jsFrameworks/my-react/src/components/portfolio/list/PortfolioListCreateItem.js
+++ 07_jsFrameworks/my-react/src/components/portfolio/list/PortfolioListCreateItem.js
@@ -14,6 +14,29 @@ export default class PortfolioListCreateItem extends React.Component {
         this.changeVisible = this.changeVisible.bind(this)
         this.onSave = this.onSave.bind(this)
         this.onInputChange = this.onInputChange.bind(this)
+        this.onUploadFile = this.onUploadFile.bind(this)
+        this.saveDataFile = this.saveDataFile.bind(this)
+
+    }
+
+    saveDataFile(data){
+        const state = this.state
+        state.portfolio['imgBlob'] = data
+        this.setState(state)
+    }
+
+    onUploadFile(ev){
+        if(FileReader){
+            let fileReader = new FileReader()
+            const save = this.saveDataFile
+            fileReader.onload = function () {
+                save(fileReader.result)
+            }
+            fileReader.onerror = function (err) {
+                console.log(err)
+            }
+            fileReader.readAsDataURL(ev.target.files[0])
+        }
     }
 
     changeVisible(){
@@ -41,9 +64,17 @@ export default class PortfolioListCreateItem extends React.Component {
         if(!this.state.isVisible) return (
             <div onClick={this.changeVisible}> show </div>
         )
+        let img
+        if(this.state.portfolio.imgBlob)
+            img = (<img src={this.state.portfolio.imgBlob} width='150px' />)
+        else
+            img = (<div> No Img </div>)
+
         return (
             <div>
+                {img}
                 <input type="text" name="name" onChange={this.onInputChange}/>
+                <input type="file" name="imgBlob" onChange={this.onUploadFile} /><br />
                 <input type="button" value="save" onClick={this.onSave}/>
                 <div onClick={this.changeVisible}> hide </div>
             </div>
