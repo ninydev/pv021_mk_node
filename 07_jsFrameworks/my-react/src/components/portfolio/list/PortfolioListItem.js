57ec995f8def80c5e78c40ce42cb34b5376fd70a
Oleksandr Nykytin <keeper@ninydev.com>
2022-04-27 18:53:39 +0300

    Portfolio Update

--- 07_jsFrameworks/my-react/src/components/portfolio/list/PortfolioListItem.js
+++ 07_jsFrameworks/my-react/src/components/portfolio/list/PortfolioListItem.js
@@ -2,10 +2,66 @@ import React from "react";
 
 
 export default class PortfolioListItem extends React.Component {
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isEdit : false,
+            portfolio: props.portfolio
+        }
+        this.onChangeEdit = this.onChangeEdit.bind(this)
+        this.onSave = this.onSave.bind(this)
+        this.onCancel = this.onCancel.bind(this)
+        this.onInputChange = this.onInputChange.bind(this)
+    }
+
+    onInputChange(ev){
+        const state = this.state
+        const name = ev.target.name
+        state[name] = ev.target.value
+        this.setState (state)
+    }
+
+    onChangeEdit(){
+        const state = this.state
+        state.isEdit = !state.isEdit
+        this.setState(state)
+    }
+
+    onSave(){
+        // this.props.portfolio.name = this.state.name
+        const state = this.state
+        state.portfolio.name = state.name
+        this.setState (state)
+        this.onCancel()
+    }
+
+    onCancel(){
+        const state = this.state
+        state.isEdit = false
+        this.setState(state)
+    }
+
     render() {
+        if(this.state.isEdit) return this.renderEdit()
         return (
             <div>
                 <h2> {this.props.portfolio.name} </h2>
+                <input type='button' onClick={this.onChangeEdit}/>
+            </div>
+        )
+    }
+    renderEdit(){
+        return (
+            <div>
+                <input type='text'
+                       name='name'
+                       onChange={this.onInputChange}
+                       defaultValue={this.props.portfolio.name}/>
+                <button type='button' onClick={this.onSave} >Save </button>
+                <button type='button' onClick={this.onCancel} >Cancel </button>
+
             </div>
         )
     }
--- 07_jsFrameworks/my-react/src/layouts/Header.js
+++ 07_jsFrameworks/my-react/src/layouts/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
 import MenuMain from "./MenuMain";
 import {Link} from "react-router-dom";
+import imgLogo from "../assets/img/logo.svg"
 
 export default class Header extends React.Component {
     render() {
         return (
-            <header> <Link to="/">Мой сайт</Link>
+            <header> <Link to="/">
+                <img src={imgLogo} width="50px" height="50px" alt="My Site"/>
+            </Link>
                 <MenuMain></MenuMain>
             </header>
         )
