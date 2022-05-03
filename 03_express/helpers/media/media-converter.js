a7d66cd28055a944936f914230d7d7ee8fe7a6ba
Oleksandr Nykytin <keeper@ninydev.com>
2022-05-03 21:03:33 +0300

    BackEnd Media Helper - To WebP

--- 03_express/helpers/media/media-converter.js
+++ 03_express/helpers/media/media-converter.js
@@ -10,7 +10,7 @@ const im = require('imagemagick');
  * @param request - передаю запрос, в котором будет файл
  * @returns - пока не сообщаю
  */
-async function convertWebP(request) {
+function convertWebP(request) {
     let file = request.file;
 
     // console.log("File: ")
@@ -21,6 +21,7 @@ async function convertWebP(request) {
     }
 
     let fromFile = path.join(__dirname, '../../uploads/', file.filename)
+    let toFile = path.join(__dirname, '../../public/uploads', file.filename) + ".webp"
     console.log("FromFile: " + fromFile)
 
     // im.resize({
@@ -36,23 +37,38 @@ async function convertWebP(request) {
     // });
 
 
-    const result = webp.cwebp(fromFile,
-        fromFile + ".webp","-q 80",logging="-v");
-    result.then((response) => {
-        console.log(response);
-    });
+    const result = webp.cwebp(fromFile, toFile,"-q 80",logging="-v");
+    // console.log(result)
+    // result.then((response) => {
+    //     // console.log(response);
+    //     return 201
+    // });
 
-
+    return 201
 }
 
+
+// avatar - 100x100
 exports.avatar = function (request, response) {
-    convertWebP(request)
+    let resultImg = convertWebP(request)
+    console.log("Code return " + resultImg)
+    if( resultImg === 201) {
+        response.send(JSON.stringify({filename: "/uploads/" + request.file.filename + ".webp"}))
+    }
+    response.statusCode = resultImg
 }
 
-
+// post - thumb - 256x256, holder - 1200x256
 exports.postThumbnail  = function (request, response) {
 }
 
+// images thumb 256x256, big 1204x1024, original
+// fileName + _imgType + ex
+// {
+//     thumb: "a3a44a3a3a2a3a_thumb.webp",
+//     big: "a3a44a3a3a2a3a_big.webp",
+//     full: "a3a44a3a3a2a3a_full.webp"
+// }
 
 exports.postImages = function (request, response) {
 }
